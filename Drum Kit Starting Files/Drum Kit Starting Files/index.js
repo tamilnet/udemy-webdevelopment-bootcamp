dad0228871f51f3220ff27cdb9dc47504b20271c
Tamil Prabha <me.tamilprabha@gmail.com>
2022-02-16 20:00:09 +0530

    Drumkit-AddEventListeners

---
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,15 @@
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i=0; i<numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+
+
+
+
+function handleClick() {
+  alert("I Got Clicked!");
+}
+}
