51fed0f071acd1b977d80c6257e80984d3ddf0ba
Tamil Prabha <prabha.58.ece@gmail.com>
2022-04-27 21:15:47 +0530

    working-with-routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,7 +1,20 @@
 
+const { response } = require("express");
 const express= require("express");
 
 const app= express();
+
+app.get("/", function(req, res){
+  res.send("<h1>Hello, there!</h1>");
+});
+
+app.get("/contact", function(req, res){
+  res.send("Contact me at: prabha@gmail.com");
+});
+
+app.get("/about", function(req, res){
+  res.send("I am a Full Stack Developer. Looking forward to learn more.");
+});
 
 app.listen(3000, function(){
   console.log("Server started running on port 3000");
