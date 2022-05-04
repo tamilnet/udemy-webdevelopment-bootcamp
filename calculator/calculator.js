b9e55d0c5e72f3e2825d5165ec70a43ef307d21c
Tamil Prabha <prabha.58.ece@gmail.com>
2022-05-04 14:10:45 +0530

    responding to requests with html

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -5,7 +5,7 @@ const express = require ("express");
 const app = express();
 
 app.get("/" , function(req , res){
-  res.send("Hello world");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000 , function(){
