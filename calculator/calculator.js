1ffcf7745db13663d80abe54db957ee7d15d7947
Tamil Prabha <prabha.58.ece@gmail.com>
2022-06-05 11:58:01 +0530

    post requests with body parser

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,32 @@
 
 
 const express = require ("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+function prettyJSON(obj) {
+  console.log(JSON.stringify(obj, null, 2));
+}
 
 app.get("/" , function(req , res){
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/" , function(req , res){
+
+  prettyJSON(req.body);
+  console.log("num1 "+ req.body.num1);
+
+  var num1 = Number(req.body.num1);
+
+  var num2= Number(req.body.num2);
+
+  var result = num1 + num2;
+
+  res.send("The result of the calculation is " + result);
 });
 
 app.listen(3000 , function(){
