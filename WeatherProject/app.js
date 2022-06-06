2e486e0534ae903ee185d165ca3628446d368eed
Tamil Prabha <prabha.58.ece@gmail.com>
2022-06-06 05:52:44 +0530

    added features at get requests

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -3,11 +3,17 @@ const express = require("express");
 
 const https = require("https");
 
+const fs = require("fs");
+
 const app = express();
+
+const openWeatherAPIKey= fs.readFileSync(__dirname + "/secrets.json" ,"utf8");
 
 app.get("/" , function(req , res){
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?city.name=chennai&appid=9b81e96cefb03c5433ba1f85d6093614&units=metric&lat=12.8833799&lon=80.131692" ;
+  
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?city.name=chennai&appid="+openWeatherAPIKey+"&units=metric&lat=12.8833799&lon=80.131692" ;
 
   https.get(url , function(response){
 
