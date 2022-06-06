8d035c02442bf6fa50f24ebe4c76ca9237eb914a
Tamil Prabha <prabha.58.ece@gmail.com>
2022-06-06 20:43:48 +0530

    parsing-JSON

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -7,19 +7,31 @@ const fs = require("fs");
 
 const app = express();
 
-const openWeatherAPIKey= fs.readFileSync(__dirname + "/secret.json" ,"utf8");
+const secretContent= fs.readFileSync(__dirname + "/secret.json" ,"utf8");
 
 app.get("/" , function(req , res){
 
-  
+  secretObject = JSON.parse(secretContent);
+  openWeatherAPIKey = secretObject.open_weather_api_key
+  console.log(openWeatherAPIKey);
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?city.name=chennai&appid="+openWeatherAPIKey+"&units=metric&lat=12.8833799&lon=80.131692" ;
+  const url = "https://api.openweathermap.org/data/2.5/weather?&appid="+openWeatherAPIKey+"&units=metric&lat=12.8833799&lon=80.131692" ;
 
   https.get(url , function(response){
 
-    console.log(response);
+    console.log(response.statusCode);
 
-  })
+    response.on("data" , function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      console.log(temp);
+      console.log(weatherDescription);
+
+        });
+
+    
+  });
 
 res.send("Server is up and running.");
 
