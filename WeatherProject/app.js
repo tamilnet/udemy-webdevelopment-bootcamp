c52b81c2784cf2cdcef2da3401c4f8cda32bb3c1
Tamil Prabha <prabha.58.ece@gmail.com>
2022-06-07 06:57:06 +0530

    render a website with live data

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -25,15 +25,22 @@ app.get("/" , function(req , res){
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png" ;
       console.log(temp);
       console.log(weatherDescription);
 
-        });
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
 
-    
+      res.write("<h1>The temperature in Chennai is " + temp + " degrees celsius.</h1>");
+
+      res.write("<img src=" + imageURL + ">"); 
+
+      res.send();
+
+    });
+
   });
-
-res.send("Server is up and running.");
 
 });
 
