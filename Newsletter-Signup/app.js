0a6ad55ed558cc47ed388a4631fb6f748d4b4b0a
Tamil Prabha <prabha.58.ece@gmail.com>
2022-06-07 20:28:47 +0530

    parsing POST requests to the server

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,24 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000 , function(){
+
+  console.log("Server started running on port 3000. ");
+
+});
--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,46 +1,74 @@
-
 const express = require("express");
 
 const https = require("https");
 
 const fs = require("fs");
 
+const bodyParser = require("body-parser");
+
 const app = express();
 
-const secretContent= fs.readFileSync(__dirname + "/secret.json" ,"utf8");
+const secretContent = fs.readFileSync(__dirname + "/secret.json", "utf8");
 
-app.get("/" , function(req , res){
+const secretObject = JSON.parse(secretContent);
+const openWeatherAPIKey = secretObject.open_weather_api_key;
+  
 
-  secretObject = JSON.parse(secretContent);
-  openWeatherAPIKey = secretObject.open_weather_api_key
-  console.log(openWeatherAPIKey);
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?&appid="+openWeatherAPIKey+"&units=metric&lat=12.8833799&lon=80.131692" ;
 
-  https.get(url , function(response){
+
+  
+
+
+app.get("/", function (req, res) {
+
+  res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function (req, res) {
+
+  
+
+  const latitude = req.body.Lat;
+
+  const longitude = req.body.Lon;
+
+  console.log(latitude);
+  console.log(longitude);
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?&appid=" + openWeatherAPIKey + "&units=metric&lat=" + latitude + "&lon=" + longitude;
+
+  https.get(url, function (response) {
 
     console.log(response.statusCode);
 
-    response.on("data" , function(data){
+    response.on("data", function (data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png" ;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       console.log(temp);
       console.log(weatherDescription);
 
       res.write("<p>The weather is currently " + weatherDescription + "</p>");
 
-      res.write("<h1>The temperature in Chennai is " + temp + " degrees celsius.</h1>");
+      res.write("<h1>The temperature in your city is " + temp + " degrees celsius.</h1>");
 
-      res.write("<img src=" + imageURL + ">"); 
+      res.write("<img src=" + imageURL + ">");
 
       res.send();
 
     });
 
   });
+
+
+
 
 });
 
@@ -51,8 +79,9 @@ app.get("/" , function(req , res){
 
 
 
-app.listen(3000 , function(){
 
-console.log("Server started running on port 3000.");
+app.listen(3000, function () {
+
+  console.log("Server started running on port 3000.");
 
 });
